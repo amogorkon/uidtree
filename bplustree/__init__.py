124f6ef2421cd7f62cd03332ad2e84ce8c85e564
Anselm Kiefner <github@anselm.kiefner.de>
2025-01-19 23:35:20 +0100

    refactor imports for clarity and consistency

--- bplustree/__init__.py
+++ bplustree/__init__.py
@@ -1,7 +1,8 @@
-from .tree import BPlusTree
-from .serializer import (
-    IntSerializer, StrSerializer, UUIDSerializer, DatetimeUTCSerializer
-)
 from .const import VERSION
+from .serializer import DatetimeUTCSerializer as DatetimeUTCSerializer
+from .serializer import IntSerializer as IntSerializer
+from .serializer import StrSerializer as StrSerializer
+from .serializer import UUIDSerializer as UUIDSerializer
+from .tree import BPlusTree as BPlusTree
 
 __version__ = VERSION
