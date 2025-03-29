d5bba05cdcbc4db649d3a82df5119982d38ed2f7
Anselm Kiefner <github@anselm.kiefner.de>
2025-03-29 16:55:47 +0100

    added WIP test, baseline test_ and tdd_test

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+"""WIP tests."""
+
+import inspect
+
+from uidtree import tree as tree
+
+# ===========================================
+
+
+# ===========================================
+
+for name, func in globals().copy().items():
+    if name.startswith("test_"):
+        print(f" ↓↓↓↓↓↓↓ {name} ↓↓↓↓↓↓")
+        print(inspect.getsource(func))
+        func()
+        print(f"↑↑↑↑↑↑ {name} ↑↑↑↑↑↑")
+        print()
